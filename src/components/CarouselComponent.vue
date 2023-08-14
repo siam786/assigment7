a57a3a1fc8ebf1f475667fe8dcbcdb173aad6203
siam786e <shohrab.hossain36@gmail.com>
2023-08-14 18:14:01 +0600

    fix carousel

--- src/components/CarouselComponent.vue
+++ src/components/CarouselComponent.vue
@@ -1,13 +1,13 @@
 <script setup>
 import { onBeforeUnmount, onMounted, ref } from "vue";
-let carousel = null;
+
 const images = ref([
   "https://images.pexels.com/photos/2518893/pexels-photo-2518893.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   "https://images.pexels.com/photos/4702634/pexels-photo-4702634.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   "https://images.pexels.com/photos/5677794/pexels-photo-5677794.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   "https://images.pexels.com/photos/6341944/pexels-photo-6341944.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
 ]);
-
+let carousel = null;
 onMounted(() => {
   carousel = new Flickity(".main-carousel", {
     autoPlay: 3000, // Set autoplay duration to 3 seconds (3000 milliseconds)
